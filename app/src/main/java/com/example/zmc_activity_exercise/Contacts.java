42c78742b13102985426b16282acec000771ae3a
Summerdise <350464517@qq.com>
2020-08-14 11:06:22 +0800

    [zmc] feat: add contacts jump to second activity function

--- app/src/main/java/com/example/zmc_activity_exercise/Contacts.java
+++ app/src/main/java/com/example/zmc_activity_exercise/Contacts.java
@@ -2,7 +2,12 @@ package com.example.zmc_activity_exercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Contacts extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class Contacts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        ImageView getContacts = findViewById(R.id.contacts_image);
+        TextView nameText = findViewById(R.id.contacts_name);
+        final String name = nameText.getText().toString();
+        TextView phoneText = findViewById(R.id.contacts_phone);
+        final String phone = phoneText.getText().toString();
+        getContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Contacts.this,Second_Activity2.class);
+                intent.putExtra("name",name);
+                intent.putExtra("phone",phone);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/zmc_activity_exercise/Second_Activity2.java
+++ app/src/main/java/com/example/zmc_activity_exercise/Second_Activity2.java
@@ -2,7 +2,10 @@ package com.example.zmc_activity_exercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Second_Activity2 extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Second_Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_2);
+
+        TextView chooseContacts = findViewById(R.id.choose_contacts);
+        chooseContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("jump.contacts");
+                startActivity(intent);
+            }
+        });
     }
 }
