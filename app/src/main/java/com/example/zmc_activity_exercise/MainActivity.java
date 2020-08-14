33ddd79fe5da2558210b072f01a6dfc6bab28dbe
Summerdise <350464517@qq.com>
2020-08-14 10:11:31 +0800

    [zmc] feat: add contacts activity

--- app/src/main/java/com/example/zmc_activity_exercise/MainActivity.java
+++ app/src/main/java/com/example/zmc_activity_exercise/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.zmc_activity_exercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView secondActivityEntry = findViewById(R.id.second_activity_entry);
+        secondActivityEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Second_Activity2.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
